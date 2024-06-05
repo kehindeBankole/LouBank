fb82626611c66a414282379c47546f135b1b12c2
kehindeBankole <bankolek1@gmail.com>
2024-06-05 22:09:09 +0100

    project set up

---
+++ LouBank/Extensions/Gradient.swift
@@ -0,0 +1,19 @@
+//
+//  Gradient.swift
+//  LouBank
+//
+//  Created by Kehinde Bankole on 05/06/2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension LinearGradient {
+    static let linearDark = LinearGradient(
+        gradient: Gradient(colors: [Color.red, Color.blue]),
+        startPoint: .top,
+        endPoint: .bottom
+    )
+
+
+}
