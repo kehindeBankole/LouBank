5a1ccd7637cd850bb1b67c230804c6b7c3897d91
kehindeBankole <bankolek1@gmail.com>
2024-06-05 20:09:01 +0100

    Initial Commit

---
+++ LouBank/LouBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  LouBankApp.swift
+//  LouBank
+//
+//  Created by Kehinde Bankole on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LouBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
